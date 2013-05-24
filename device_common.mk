f9f1e6c791780a6e4fff69c8dea68ac7af89ec0c
Henric Andersson <henric.andersson@sonymobile.com>
2013-05-23 18:27:16 -0700

    Cleanup of how we make the device mk files
    Less duplication = less chance of bugs
    
    Change-Id: Ic470c218d2023b59fac8185c9faa00a2ac171ed2

--- device_common.mk
+++ device_common.mk
@@ -1,6 +1,23 @@
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+# Platform specific overlays
+#
 DEVICE_PACKAGE_OVERLAYS += device/sony/lagan/overlay
 
+# Platform specific packages
+#
 PRODUCT_PACKAGES += \
     Nfc \
     Tag \
@@ -12,20 +29,32 @@ PRODUCT_PACKAGES += \
     audio_policy.msm8960 \
     com.android.nfc_extras \
     e2fsck \
-    libaudio-resampler \
-    libnfc \
-    libnfc_jni \
     gps.msm8960 \
+    libaudio-resampler \
     libgps.utils \
     libloc_adapter \
     libloc_eng \
-    sensors.default
+    libnfc \
+    libnfc_jni \
+    p2p_supplicant.conf \
+    sensors.default \
 
+# Platform specific tags
+#
 PRODUCT_TAGS += dalvik.gc.type-precise
 
+# Platform specific properties
+#
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.qualcomm.bt.hci_transport=smd \
 
+# Platform specific default properties
+#
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+# Platform specific files
+#
 SONY_ROOT = device/sony/lagan/rootdir
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/fstab.sony:root/fstab.sony \
@@ -50,3 +79,25 @@ PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/system/usr/keylayout/pmic8xxx_pwrkey.kcm:system/usr/keylayout/pmic8xxx_pwrkey.kcm \
     $(SONY_ROOT)/system/usr/keylayout/pmic8xxx_pwrkey.kl:system/usr/keylayout/pmic8xxx_pwrkey.kl \
     $(SONY_ROOT)/ueventd.sony.rc:root/ueventd.sony.rc \
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.google.android.nfc_extras.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+
--- device_tablet.mk
+++ device_tablet.mk
@@ -1,10 +1,30 @@
-# Include common and then apply telephony baseline
+# Copyright (C) 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Include common and then apply bare minimum to use modem. This is necessary
+# since there is no support in AOSP for a tablet which needs ril for connectivity.
 #
 $(call inherit-product, device/sony/lagan/device_common.mk)
 
-############################################################################################
-#
-# We still need the ril, apn and some other stuff, so copy that from full_base_telephony.mk
+# We are tablet
+PRODUCT_CHARACTERISTICS := tablet
+
+# We need hardware
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
+
+# We need the ril and some properties, so copy that from full_base_telephony.mk
 # If you look at that file, you'll see that this is the order things are done.
 #
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -12,8 +32,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=true
 
 PRODUCT_PACKAGES += rild
-#
-############################################################################################
 
+# Carry on
+#
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 $(call inherit-product-if-exists, vendor/google/products/gms.mk)

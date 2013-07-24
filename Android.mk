26ca349575e7d7e781e048c31670c824773b8346
Johan Redestig <johan.redestig@sonymobile.com>
2013-07-24 20:21:53 +0200

    Take wpa_supplicant produced from hardware/qcom/wlan
    Change-Id: Ie60c288654de726f67cceb12c563cbca1df02ced

--- Android.mk
+++ Android.mk
@@ -28,14 +28,6 @@ ifneq ($(filter c6603 c6503 c5503 sgp321 sgp311,$(TARGET_DEVICE)),)
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := wpa_supplicant.conf
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := p2p_supplicant.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
